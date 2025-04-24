23f9ffbbb07722dc2b073383de559c7b89f60979
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-24 14:42:18 +0530

    Create program008.py

---
+++ program008.py
@@ -0,0 +1,22 @@
+# program008 :
+
+# Write a program that accepts a comma separated sequence of words as
+# input and prints the words in a comma-separated sequence after sorting
+# them alphabetically.
+
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+
+# Then, the output should be:
+# bag,hello,without,world
+
+
+def main(data):
+    mylist = data.split(",")
+    mylist.sort()
+    data = ','.join(mylist)
+    return data
+
+if __name__ == "__main__":
+    data = input()
+    print(main(data))
