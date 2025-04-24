689fc9ea8fb64446c547834762229f7bb9e3486b
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-24 14:09:50 +0530

    program1

---
+++ program001.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# program001 :
+
+# Define the program name
+# Write a program which will find all such numbers which are
+# divisible by 7 but are not a multiple of 5, between 2000
+# and 3200 (both included).
+
+# The numbers obtained should be printed in a comma-separated
+# sequence on a single line.
+
+# Hints:
+# Consider use range(#begin, #end) method
+
+# all logic should be in side main method only
+
+
+import logging
+
+def main(start, end): #created main function
+    mylist = []  # created empty list
+    for i in range(start, end): # started for loop for the given start and end
+        if i % 7 == 0 and i % 5 != 0: # logic for the output
+            mylist.append(str(i)) # append the data to list in string form
+    return ",".join(i for i in mylist) # converting list in to comma seprated values
+
+if __name__ == "__main__":
+    try:
+        start = int(input("Enter first number : "))
+        end = int(input("Enter last number : "))
+        print("The out put is {}".format(main(start,end+1)))
+    except:
+        logging.warning("\tcheck value")
