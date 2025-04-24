cf7d864046cfa53addca75fb8f1db2148bf3b6d6
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-24 14:11:00 +0530

    Create program002.py

---
+++ program002.py
@@ -0,0 +1,30 @@
+# program002 :
+
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+
+
+import logging
+
+def main(number):
+    if number == 1:
+        return 1
+    return number * main(number-1)
+    # count = 1
+    # while number > 1:
+    #     count = count * number
+    #     number -= 1
+    # return count
+    
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter the number : "))
+        print("The factorial of the number {0} is : {1}".format(number , main(number)))
+    except:
+        logging.warning("check value")
