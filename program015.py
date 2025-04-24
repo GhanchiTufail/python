632efadc7737facb4214c86b3d84d0d6ed402b8f
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-24 16:34:24 +0530

    Create program015.py

---
+++ program015.py
@@ -0,0 +1,25 @@
+# program 15 :
+
+# Write a program that computes the value of a+aa+aaa+aaaa
+# with a given digit as the value of a.
+# Suppose the following input is supplied to the program:
+# 9
+# Then, the output should be:
+# 11106
+
+
+def main(data):
+    k = data
+    k1 = k + (data * 10)
+    k2 = k1 + (data * 100)
+    k3 = k2 + (data * 1000)
+    num = k + k1 + k2 + k3
+    return num
+
+
+if __name__ == "__main__":
+    try:
+        data = int(input("Enter the number : "))
+        print(main(data))
+    except:
+        ValueError("check value")
