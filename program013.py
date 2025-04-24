be63126a74e4d8e5d9e5d12bc636b5ccadcf0bee
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-24 16:22:22 +0530

    program 13 and 14

---
+++ program013.py
@@ -0,0 +1,30 @@
+# program 13 :
+
+# Write a program that accepts a sentence and
+# calculate the number of letters and digits.
+# Suppose the following input is supplied to the program:
+# hello world! 123
+# Then, the output should be:
+# LETTERS 10
+# DIGITS 3
+
+
+def main(string):
+    digits = 0
+    characters = 0
+    for i in string:
+        if i.isdigit():
+            digits += 1
+        if i.isalpha():
+            characters += 1
+    return {
+        "Digits":digits,
+        "Alphabets":characters
+    }
+
+if __name__ == "__main__":
+    try:
+        string = input("Enter the sentence : ")
+        print(main(string))
+    except:
+        ValueError("Check input")
---
+++ program014.py
@@ -0,0 +1,30 @@
+# program 14 :
+
+# Write a program that accepts a sentence and
+# calculate the number of upper case letters and lower case letters.
+# Suppose the following input is supplied to the program:
+# Hello world!
+# Then, the output should be:
+# UPPER CASE 1
+# LOWER CASE 9
+
+
+def main(sentence):
+    upper = 0
+    lower = 0
+    for i in sentence:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+    return {
+        "Uppercase":upper,
+        "Lowercase":lower
+    }
+
+if __name__ == "__main__":
+    try:
+        string = input("Enter the sentence : ")
+        print(main(string))
+    except:
+        ValueError("Check input")
