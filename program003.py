fc9da93f1f5e099f05a8272bc4d2b00a028d4107
GhanchiTufail <tufailghanchi2022@gmail.com>
2025-04-25 12:22:56 +0530

    Update program003.py

--- program003.py
+++ program003.py
@@ -10,9 +10,6 @@
 # Then, the output should be:
 # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 
-
-import logging
-
 def main(number):
     dict1 = {}
     for i in range(1,number+1):
@@ -24,4 +21,4 @@ if __name__ == "__main__":
         number = int(input("Enter the number : "))
         print(main(number))
     except:
-        logging.warning("check value")
+        print("check value")
